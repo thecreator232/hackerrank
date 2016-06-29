e9f7e4f78393fa1d2a5705631b512158106bfcc3
thecreator232 <thecreator232@gmail.com>
2016-06-29 23:12:12 +0530

    Insert a Node at the Tail of a Linked List

--- python/data structure/linked lists/insert_a_node_at_the_tail_of_a _linked_list.py
+++ python/data structure/linked lists/insert_a_node_at_the_tail_of_a _linked_list.py
@@ -1,1 +1,32 @@
 __author__ = 'thecreator232'
+"""
+ Insert Node at the end of a linked list
+ head pointer input could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method
+"""
+
+
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+def Insert(head, data):
+    try:
+        if head.next is not None:
+            Insert(head.next, data)
+        else:
+            node = Node(data=data)
+            head.next = node
+    except AttributeError:
+        head = Node(data=data)
+    return head
